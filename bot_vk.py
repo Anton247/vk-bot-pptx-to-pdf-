bcff1b647283b3217f60f346fae7c4b92c79b975
Anton <kondratov247@mail.ru>
2021-09-24 23:41:23 +0700

    Summary

--- bot_vk.py
+++ bot_vk.py
@@ -12,66 +12,91 @@ import sqlite3
 import logging
 from vkbottle.bot import Bot, Message
 from vkbottle.tools import DocMessagesUploader
-from setting import TOKEN
+from settings import TOKEN
 
-logging.basicConfig(level="DEBUG")
+from morphy_using import name_change
+
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 
 keyboard = '{"buttons":[[{"action":{"type":"text","label":"Хочу сертификат","payload":""},"color":"positive"}]]}'
+empty_keyboard = '{"buttons":[]}'
+mess = {}  #Тут будем хранить время последнего обращения к боту от пользователя
+# так делать плохо и лучше БД использовать, но сейчас пойдёт
 
 @bot.on.message(text="Начать")
 async def hi_handler(message: Message):
+    print()
+    print(message.from_id)
+    print()
     users_info = await bot.api.users.get(message.from_id)
-    S = "Привет, {}".format(users_info[0].first_name)
+    print()
+    print(users_info)
+    print()
+    print(users_info[0].first_name)
+    print()
+    S = "Привет, " + users_info[0].first_name
     S += "\n👋🏼😀\nЯ Квантоша, бот, созданный для отправки сертификата о посещении дня открытых дверей\
         \nНапиши мне своё имя и я отправлю тебе твой сертификат"
+    
     await message.answer(S, keyboard=keyboard)
 
 @bot.on.message(text="Хочу сертификат")
 async def what_is_name(message: Message):
-    await message.answer("Напиши мне свои ФИО")
+    await message.answer("Напиши мне свои ФИО", keyboard=empty_keyboard)
 
 @bot.on.message()
 async def certificate(message: Message):
-    await message.answer("Твой сертификат создаётся, подожди немного", keyboard=keyboard)
-    now = str(datetime.date.today().day)
-    now += "-" + str(datetime.date.today().month)
-    now += "-" + str(datetime.date.today().year)
-    UID = uuid.uuid4().hex #уникальный идентификатор
-    file = PPTX_GENERATOR(message.text, UID, now)
-    print("GENERATOR OK")
-    file = file.replace(" ", "©")
-    command = "python PPTX_to_PDF.py " + file + " " + now
-    print("открываем скрипт для форматирования")
-    res = os.system(command)  # открываем скрипт для форматирования
-    file = file.replace("©", " ")
-    print("PDF OK")
-    doc = await DocMessagesUploader(bot.api).upload(
-        file + ".pdf", './GENERATED_PDF/' + now + '/' + file + ".pdf", peer_id=message.peer_id
-    )
-    print("DOK OK")
-    await message.answer(attachment=doc)
-
-    connect = sqlite3.connect('users.db')
-    cursor = connect.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users(
-			user_id TEXT PRIMARY KEY,
-			user_name TEXT,
-			date TEXT,
-			time TEXT,
-			source TEXT,
-			uname_source TEXT,
-			uid_source TEXT
-   			)
-    		""")	
-    now_time = datetime.datetime.now()
+    global mess  # время последнего обращения к боту от пользователя (словарь)
     users_info = await bot.api.users.get(message.from_id)
-    print("INFOOOOOOOOOOOO")
-    print(users_info)
-    users_list = [UID, message.text, now, now_time.strftime("%H:%M:%S"), "VK", users_info[0].first_name + " " + users_info[0].last_name, users_info[0].id]
-    cursor.execute("INSERT INTO users VALUES(?,?,?,?,?,?,?);", users_list)
-    connect.commit()
+    #  "генийальный" анти DDoS
+    can_receive_message = 1
+    print()
+    print("MESS:", mess)
+    print()
+    if users_info[0].id not in mess: #Если пользователь не писал ещё сообщения, то добавляем его ID в словарь и присваиваем время
+        mess[users_info[0].id] = datetime.datetime.now()
+    elif (datetime.datetime.now() - mess[users_info[0].id]).total_seconds() < 15:  # Ставим ограничения на время последовательных сообщений боту
+        await message.answer('Мне можно писать не чаще чем раз в 15 секунд\nಥ_ಥ\nТебе придётся подождать')
+        can_receive_message = 0
+    if can_receive_message:
+        await message.answer("Твой сертификат создаётся, подожди немного", keyboard=keyboard)
+        now = str(datetime.date.today().day)
+        now += "-" + str(datetime.date.today().month)
+        now += "-" + str(datetime.date.today().year)
+        UID = uuid.uuid4().hex #уникальный идентификатор
+        user_name = name_change(message.text)
+        file = PPTX_GENERATOR(user_name, UID, now)
+        print("GENERATOR OK")
+        file = file.replace(" ", "©")
+        command = "python PPTX_to_PDF.py " + file + " " + now
+        print("открываем скрипт для форматирования")
+        res = os.system(command)  # открываем скрипт для форматирования
+        file = file.replace("©", " ")
+        print("PDF OK")
+        doc = await DocMessagesUploader(bot.api).upload(
+            file + ".pdf", './GENERATED_PDF/' + now + '/' + file + ".pdf", peer_id=message.peer_id
+        )
+        print("DOK OK")
+        await message.answer(attachment=doc)
+
+        connect = sqlite3.connect('users.db')
+        cursor = connect.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+                user_id TEXT PRIMARY KEY,
+                user_name TEXT,
+                date TEXT,
+                time TEXT,
+                source TEXT,
+                uname_source TEXT,
+                uid_source TEXT
+                )
+                """)	
+        now_time = datetime.datetime.now()
+        users_list = [UID, message.text, now, now_time.strftime("%H:%M:%S"), "VK", users_info[0].first_name + " " + users_info[0].last_name, users_info[0].id]
+        cursor.execute("INSERT INTO users VALUES(?,?,?,?,?,?,?);", users_list)
+        connect.commit()
 
 bot.run_forever()
 
